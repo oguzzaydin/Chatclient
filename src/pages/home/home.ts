11efe6337cd8b43d944e06267fa93f785c8ba667
Oğuzhan Aydın <oguzhanaydin56@gmail.com>
2018-01-23 15:47:44 +0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Socket} from 'ng-socket-io'
 import {Observable} from 'rxjs/Observable'
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'page-home',
@@ -12,33 +13,59 @@ export class HomePage {
   nickname='';
   message='';
   user='';
-  userList=[];
+  userList=Array<{userName:string,id:string}>();
   socketId='';
+  selectedUser={user:{},id:''};
 
   constructor(public navCtrl: NavController,
   private socket:Socket
-  ) {  }
+  ) {
+
+
+    this.userListAll().subscribe(data=>{
+      let js  = JSON.stringify(data);
+      let jsparse = JSON.parse(js);
+      console.log(data);
+      for(var i =0;i<jsparse.length;i++)
+      {
+        let user_name = JSON.stringify(data[i].userName.user);
+        this.userList.push({ userName:user_name,id:data[i].id});
+      } 
+   });   
+  }
+    
   
 
   sendMessage(){
-    this.socket.emit('getMsg',{text:this.message});
-    this.message='';
+    if(this.message!=null){
+      this.socket.emit('getMsg',{
+        toid:this.selectedUser.id,
+        msg:this.message,
+        name:this.user
+      });
+    }
+    // this.socket.emit('getMsg',{text:this.message});
+    // this.message='';
+  }
+  selectUser(data){
+    this.selectedUser=data;
+    console.log(data);
   }
   
   userListAll(){
-   this.socket.emit('username',{user:this.user,socketId:'1'});
-   this.userList.push({
-     user:this.user,
-     id:this.socketId
-   }) 
-  
-  }
-
+    console.log(this.user);
+   this.socket.connect();
+   this.socket.emit('username',{user:this.user});
+  let observable = new Observable(observer=> { this.socket.on('userList',(data)=>{
+  observer.next(data);
+   });
+  });
+  return observable;
+}
   getMessages(){
     let observable=new Observable(observer=>{
       this.socket.on('message',(data)=>{
         observer.next(data);
-      
       });
     });
     return observable;
