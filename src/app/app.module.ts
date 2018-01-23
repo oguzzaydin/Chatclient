00584eb0cdc55d44f51bb5b9afc5a0fee46557db
Oğuzhan Aydın <oguzhanaydin56@gmail.com>
2018-01-23 12:32:23 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:81', options: {} };
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SocketIoModule.forRoot(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Socket} from 'ng-socket-io'
+import {Observable} from 'rxjs/Observable'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  messeges=[];
+  nickname='';
+  message='';
+  user='';
+  userList=[];
+  socketId='';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+  private socket:Socket
+  ) {  }
+  
 
+  sendMessage(){
+    this.socket.emit('getMsg',{text:this.message});
+    this.message='';
+  }
+  
+  userListAll(){
+   this.socket.emit('username',{user:this.user,socketId:'1'});
+   this.userList.push({
+     user:this.user,
+     id:this.socketId
+   }) 
+  
+  }
+
+  getMessages(){
+    let observable=new Observable(observer=>{
+      this.socket.on('message',(data)=>{
+        observer.next(data);
+      
+      });
+    });
+    return observable;
   }
 
 }
